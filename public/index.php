12bf59059f6ef437ed9e17f1e3b98bbd9c7ca2a7
mame10 <gmalick540@gmail.com>
2022-04-29 21:47:12 +0000

    trello

---
+++ public/index.php
@@ -0,0 +1,6 @@
+header(Access-Control-Allow-Origin: <origin> |*)
+Access-Control-Max-Age: <delta-en-secondes>
+Access-Control-Allow-Methods: <methode>[, <methode>]*
+Access-Control-Request-Method: <methode>
+Access-Control-Request-Headers: <nom-champ>[, <nom-champ>]*
+header(Access-Control-Expose-Headers: X-Mon-En-tete-Specifique, X-Un-Autre-En-tete)
